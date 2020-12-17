6a476925ed6f9afb8b5f0685d1c6ab0ad4bcac9e
ganeshkumar269 <mganeshkumar269@gmail.com>
2020-12-17 15:45:05 +0530

    added parentpop in original/MREA

--- original/MREA.java
+++ original/MREA.java
@@ -13,9 +13,11 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MREA{
     public static class EMapper extends  Mapper<LongWritable ,Text,IntWritable,Text>{
+        //intput -> (key, {task,csid,csqos})
+        //output -> (task,{csid,csqos})
         public void map(LongWritable key, Text value, 
         Context context) throws IOException,InterruptedException { 
-            StringTokenizer st = new StringTokenizer(value," ");
+            StringTokenizer s = new StringTokenizer(value," ");
             if(!s.nextToken().equals("solution"))
             {
                 String task = s.nextToken();
@@ -26,17 +28,23 @@ public class MREA{
             }
         }
     }
+    //output -> (1,{task,csid,csqos})
     public static class ECombiner extends  Reducer<IntWritable ,Text,IntWritable,Text>{
         public void reduce(Text key, Iterator <Text> values, 
         Context context) throws IOException,InterruptedException {    
             ArrayList<ArrayList<Double>> csList = new ArrayList<ArrayList<Double>>();
+            ArrayList<Double> cs;
+            ArrayList<Integer> csIds = new ArrayList<Integer>();
             while(values.hasNext()){
                 Text value = values.next();
                 StringTokenizer st = new StringTokenizer(value," ");
+                int csid = Integer.parseInt(st.nextToken());
+                csIds.add(csid);
+                cs = new ArrayList<Double>();
                 while(st.hasMoreTokens()){
-                    cs = new ArrayList<Double>();
                     cs.add(Double.parseDouble(st.nextToken()));
                 }
+                csList.add(cs);
             }
             //Skyline
             ArrayList<ArrayList<Double>> domList = new ArrayList<ArrayList<Double>>();
@@ -58,7 +66,7 @@ public class MREA{
                 }
                 if(canBeAdded == true){
                     String out = new String();
-                    out = out + key.toString() + " ";
+                    out = key.toString() + " " + String.valueOf(csIds.get(i)) + " ";
                     for(int k = 0 ;k < 5; k){
                         out = out + String.valueOf(csList.get(i).get(k)) + " ";
                     }
@@ -71,6 +79,8 @@ public class MREA{
         public void reduce(Text key, Iterator <Text> values, 
         Context context) throws IOException,InterruptedException { 
             ArrayList<ArrayList<Double>> csList = new  ArrayList<ArrayList<Double>>();
+            HashMap<Integer,ArrayList<ArrayList<Double>> > totalList = new  HashMap<Integer,ArrayList<Double>>(); 
+            HashMap<Integer,ArrayList<Integer>> totalids = new  HashMap<Integer,ArrayList<Double>>(); 
             for(int i = 0;i < 100;i++)
                 csList.add(new ArrayList<Double>());
             for(Text i : values){
@@ -78,26 +88,84 @@ public class MREA{
                 ArrayList<Double> cs = new ArrayList<Double>();
                 StringTokenizer st = new StringTokenizer(t, " ");
                 int task = Integer.parseInt(st.nextToken());
+                int csid = Integer.parseInt(st.nextToken());
+                
                 for(int k =0; k < 5;k++)
                     cs.add(Double.parseDouble(st.nextToken()));
-                csList.get(task).add(cs);
+                if(totalList.containsKey(task)){
+                    totalList.get(task).add(cs);
+                    totalids.get(task).add(csid);
+                }
+                else{
+                    totalList.put(task,new ArrayList<ArrayList<Double>>());
+                    totalids.put(task,new ArrayList<Integer>());
+                    totalids.get(task).add(csid);
+                    totalList.get(task).add(cs);
+                }
             }
             
             //new Solutions
             // int numOfSols = 1000;
-            ArrayList<ArrayList<Double>> sols = new ArrayList<Integer>(); 
+            ArrayList<ArrayList<Integer>> sols = new ArrayList<ArrayList<Integer>>(); 
             Random rand = new Random();
-            int numOfCS = domList.size();
-            for(int i = 0; i < numOfSols; i++){
-                sols.add(rand.nextInt(numOfCS));
+            int numOfSols = 1000;
+            int taskNum = totalList.size();
+            for(int i = 0; i < numOfSols; i++)
+            {
+                ArrayList<Integer> temp = new ArrayList<Integer>();
+                for(int j = 0; j < taskNum; j++){
+                    temp.add( rand.nextInt( totalids.get(j).size() ) );
+                }
+                sols.add( temp );
             }
             
             //Generate and Initialise Probablities 
+            ArrayList<ArrayList<Double>> prob = new ArrayList<ArrayList<Double>>();
+            for(int i =0;i < taskNum; i++){
+                ArrayList<Double> temp = new ArrayList<Double>();
+                for(int j =0 ;j < totalList.get(i)).size(); j++)
+                    temp.add(1.0/totalList.get(i).size());
+                prob.add(temp);
+            }
+
             // ArrayList<Double> prob = new ArrayList<Double>();
             // for(int i = 0; i < numOfCS; i++)
             //     prob.add(1.0/numOfCS);
             
             //Generate Parent Pop
+            ArrayList<ArrayList<Integer>> parentPop = new ArrayList<ArrayList<Integer>>(); 
+            ArrayList<Double>> fitness = new ArrayList<Double>();
+            
+            for(ArrayList<Integer> i : sols){
+                double val = 0;
+                double tempVal = 0;
+                for(int j = 0; j < taskNum;j++)
+                    tempVal += totalList.get(j).get(i.get(j)).get(0);
+                val += 0.1417*tempVal;
+                tempVal = Double.MAX_VALUE;
+                for(int j = 0; j < taskNum;j++)
+                    tempVal = min(tempVal,totalList.get(j).get(i.get(j)).get(1));
+                val += tempVal;
+                tempVal = 1;
+                for(int j = 0; j < taskNum;j++)
+                    tempVal *= totalList.get(j).get(i.get(j)).get(2);
+                val += tempVal;
+                tempVal = 1;
+                for(int j = 0; j < taskNum;j++)
+                    tempVal *= totalList.get(j).get(i.get(j)).get(3);
+                val += tempVal;
+                tempVal = 0;
+                for(int j = 0; j < taskNum;j++)
+                    tempVal += totalList.get(j).get(i.get(j)).get(4);
+                val += tempVal;
+                fitness.add(val);
+            }
+            ArrayList<Double> temp_fit = new ArrayList<Double>(fitness);
+            Collections.sort(temp_fit);
+            double th = temp_fit.get(3*fitness.size()/4);
+            for(ArrayList<Integer> i : sols){
+                if()
+            }
 
             //Update Prob
             //Guided Mutation
